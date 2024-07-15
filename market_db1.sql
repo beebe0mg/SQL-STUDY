98e4e5fda40112f67fe39b476c2ed35c380ef492
정상미 <w2321@e-mirim.hs.kr>
2024-07-15 23:25:10 +0900

    PREPARE과 EXECUTE 사용하기

--- market_db1.sql
+++ market_db1.sql
@@ -8,3 +8,7 @@ SELECT @myVar1 + @myVar2 ;
 SET @txt = '가수 이름 ==> ' ;
 SET @height = 166 ;
 SELECT @txt , mem_name FROM member WHERE height > @height ;
+
+SET @count = 3;
+PREPARE mySQL FROM 'SELECT mem_name, height FROM member ORDER BY height LIMIT ?';
+EXECUTE mySQL USING @count;
