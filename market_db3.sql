ffe66029fbb6b99312faebc7ec9463f5b70ec38f
정상미 <w2321@e-mirim.hs.kr>
2024-07-23 23:59:12 +0900

    간단한 뷰 재생성

--- market_db3.sql
+++ market_db3.sql
@@ -10,15 +10,19 @@ USE market_db;
 -- SELECT mem_name, addr FROM V_member
 --	WHERE addr IN ('서울', '경기');
 
-ALTER VIEW v_viewtest1
-AS
-	SELECT B.mem_id '회원 아이디', M.mem_name AS '회원 이름',
-    B.prod_name "제품 이름",
-		CONCAT(M.phone1, M.phone2) AS "연락처"
-	FROM buy B
-		INNER JOIN member M
-        ON B.mem_id = M.mem_id;
-        
-SELECT DISTINCT `회원 아이디`, `회원 이름` FROM v_viewtest1;
+-- ALTER VIEW v_viewtest1
+-- AS
+-- 	SELECT B.mem_id '회원 아이디', M.mem_name AS '회원 이름',
+--     B.prod_name "제품 이름",
+-- 		CONCAT(M.phone1, M.phone2) AS "연락처"
+-- 	FROM buy B
+-- 		INNER JOIN member M
+--         ON B.mem_id = M.mem_id;
+--         
+-- SELECT DISTINCT `회원 아이디`, `회원 이름` FROM v_viewtest1;
 
-DROP VIEW v_viewtest1;
+-- DROP VIEW v_viewtest1;
+
+CREATE OR REPLACE VIEW v_viewtest2
+AS
+	SELECT mem_id, mem_name, addr FROM member;
