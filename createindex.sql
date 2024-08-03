5f2fc3371cd240e0e5a95be3a90b7953b6dec0dd
정상미 <w2321@e-mirim.hs.kr>
2024-08-03 14:25:15 +0900

    인덱스 제거 실습

--- createindex.sql
+++ createindex.sql
@@ -88,3 +88,16 @@ ANALYZE TABLE member;
 SELECT mem_name, mem_number
 	FROM member
     WHERE mem_number >= 7;
+    
+DROP INDEX idx_member_mem_name ON member;
+DROP INDEX idx_member_addr ON member;
+DROP INDEX idx_member_mem_number ON member;
+
+SELECT table_name, constraint_name
+	FROM information_schema.referential_constraints
+    WHERE constraint_schema = 'market_db';
+    
+ALTER TABLE buy
+	DROP FOREIGN KEY buy_ibfk_1;
+ALTER TABLE member
+	DROP PRIMARY KEY;
