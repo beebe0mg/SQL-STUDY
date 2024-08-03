3aa3cf9c8254f9293aecf1ab4e6343800b459b8b
정상미 <w2321@e-mirim.hs.kr>
2024-08-03 10:03:08 +0900

    클러스터형 인덱스 사용해 보기

---
+++ index.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE member
+-- ( mem_id		CHAR(8) NOT NULL PRIMARY KEY,
+--   mem_name		VARCHAR(10) NOT NULL,
+--   mem_number	INT NOT NULL,
+--   addr			CHAR(2) NOT NULL,
+--   phone1		CHAR(3),
+--   phone2		CHAR(8),
+--   height		SMALLINT,
+--   debut_table	DATE
+-- );
+
+CREATE TABLE table1_new (
+    col1 INT PRIMARY KEY,
+    col2 INT,
+    col3 INT
+);
+
+
+SHOW INDEX FROM table1_new;
