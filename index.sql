6dcf3b451c3bee8c3409d659069f4c51194838a1
정상미 <w2321@e-mirim.hs.kr>
2024-08-03 10:28:32 +0900

    클러스터형으로 정렬

--- index.sql
+++ index.sql
@@ -17,10 +17,30 @@
 
 -- SHOW INDEX FROM table1_new;
 
-CREATE TABLE table2_new (
-    col1 INT PRIMARY KEY,
-    col2 INT UNIQUE,
-    col3 INT UNIQUE
+-- CREATE TABLE table2_new (
+--     col1 INT PRIMARY KEY,
+--     col2 INT UNIQUE,
+--     col3 INT UNIQUE
+-- );
+
+-- SHOW INDEX FROM table2_new;
+
+USE market_db;
+DROP TABLE IF EXISTS buy, member;
+CREATE TABLE member
+( mem_id	CHAR(8),
+  mem_name 	VARCHAR(10),
+  mem_number	INT,
+  addr		CHAR(2)
 );
 
-SHOW INDEX FROM table2_new;
+INSERT INTO member VALUES('TWC', '트와이스', 9, '서울');
+INSERT INTO member VALUES('BLK', '블랙핑크', 4, '경남');
+INSERT INTO member VALUES('WMN', '여자친구', 6, '경기');
+INSERT INTO member VALUES('OMY', '오마이걸', 7, '서울');
+SELECT * FROM member;
+
+ALTER TABLE member
+	ADD CONSTRAINT
+    PRIMARY KEY (mem_id);
+SELECT * FROM member;
