bbc352e64f5cdda3bcf409616e958e8f409fde5b
정상미 <w2321@e-mirim.hs.kr>
2024-08-03 11:50:25 +0900

    클러스터형 인덱스 구성하기

--- index.sql
+++ index.sql
@@ -75,3 +75,19 @@ SELECT * FROM member;
 
 INSERT INTO member VALUES('GRL', '소녀시대', 8, '서울');
 SELECT * FROM member;
+
+USE market_db;
+CREATE TABLE cluster
+( mem_id	CHAR(8),
+  mem_name	VARCHAR(10)
+);
+INSERT INTO cluster VALUES('TWC', '트와이스');
+INSERT INTO cluster VALUES('BLK', '블랙핑크');
+INSERT INTO cluster VALUES('WMN', '여자친구');
+INSERT INTO cluster VALUES('OMY', '오마이걸');
+INSERT INTO cluster VALUES('GRL', '소녀시대');
+INSERT INTO cluster VALUES('ITZ', '잇지');
+INSERT INTO cluster VALUES('RED', '레드벨벳');
+INSERT INTO cluster VALUES('APN', '에이핑크');
+INSERT INTO cluster VALUES('SPC', '우주소녀');
+INSERT INTO cluster VALUES('MMU', '마마무');
