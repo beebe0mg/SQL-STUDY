d10b87ede22937d05fe25902bd740b971781721a
정상미 <w2321@e-mirim.hs.kr>
2024-08-15 23:14:12 +0900

    입력 매개변수의 활용

--- procedure.sql
+++ procedure.sql
@@ -10,3 +10,24 @@ DELIMITER ;
 CALL user_proc();
 
 DROP PROCEDURE user_proc;
+
+USE market_db;
+DROP PROCEDURE IF EXISTS user_proc1;
+DELIMITER $$
+CREATE PROCEDURE user_proc1(IN userName VARCHAR(10))
+BEGIN
+	SELECT * FROM member WHERE mem_name = userName;
+END $$
+DELIMITER ;
+
+CALL user_proc1('에이핑크');
+
+DROP PROCEDURE IF EXISTS user_proc2;
+DELIMITER $$
+CREATE PROCEDURE user_proc2(IN userNumber INT, IN userHeight INT)
+BEGIN
+	SELECT * FROM member WHERE mem_number > userNumber AND height > userHeight;
+END $$
+DELIMITER ;
+
+CALL user_proc2(6, 165);
